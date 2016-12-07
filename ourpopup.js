d8bfdf9ceeedd7e42901bd45de4d36b29e761a28
Melvin Woo <MelvinWoo@Melvins-MacBook-Air.local>
2016-12-06 23:17:08 -0500

    ourpopup updated

--- ourpopup.js
+++ ourpopup.js
@@ -1,118 +1,235 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// this checks if the elements already exists on the document (exit if extension is clicked again)
+if (document.getElementById('can')) {
+    exit();
+} else {
+    var erase_drawing = false;
+    var stop = false;
+    var canvas = document.createElement("canvas");
+    // ID of the canvas object
+    canvas.id = "can";
+    // Canvas lives inside body (so document -> body -> canvas)
+    document.body.appendChild(canvas);
+    // ID of draggable object
+    
+    // # specifies that the elements are selected by their ID's
+    $("#drawingCanvas").append('</canvas>');
 
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback - called when the URL of the current tab
- *   is found.
- */
-function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
+    $("#draw_button").click(draw);
+    $("#erase_button").click(erase);
+    $("#clear_button").click(clear);
+    $("#exit_button").click(exit);
 
-  chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
-    var tab = tabs[0];
+    var draw_button = document.getElementById("draw_button");
+    var erase_button = document.getElementById("erase_button");
+    var buttons = document.getElementsByClassName("functionalities");
 
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
-
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
-
-    callback(url);
-  });
-
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, function(tabs) {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
-}
-
-/**
- * @param {string} searchTerm - Search term for Google Image search.
- * @param {function(string,number,number)} callback - Called when an image has
- *   been found. The callback gets the URL, width and height of the image.
- * @param {function(string)} errorCallback - Called when the image is not found.
- *   The callback gets a string that describes the failure reason.
- */
-function getImageUrl(searchTerm, callback, errorCallback) {
-  // Google image search - 100 searches per day.
-  // https://developers.google.com/image-search/
-  var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images' +
-    '?v=1.0&q=' + encodeURIComponent(searchTerm);
-  var x = new XMLHttpRequest();
-  x.open('GET', searchUrl);
-  // The Google image search API responds with JSON, so let Chrome parse it.
-  x.responseType = 'json';
-  x.onload = function() {
-    // Parse and process the response from Google Image Search.
-    var response = x.response;
-    if (!response || !response.responseData || !response.responseData.results ||
-        response.responseData.results.length === 0) {
-      errorCallback('No response from Google Image search!');
-      return;
+    with(canvas.style) {
+        cursor = 'crosshair'
+        top = '0px';
+        left = '0px';
+        position = 'absolute';
+        zIndex = '2147483646';
+        backgroundColor = 'transparent';
+        userSelect = 'none';
     }
-    var firstResult = response.responseData.results[0];
-    // Take the thumbnail instead of the full image to get an approximately
-    // consistent image size.
-    var imageUrl = firstResult.tbUrl;
-    var width = parseInt(firstResult.tbWidth);
-    var height = parseInt(firstResult.tbHeight);
-    console.assert(
-        typeof imageUrl == 'string' && !isNaN(width) && !isNaN(height),
-        'Unexpected respose from the Google Image Search API!');
-    callback(imageUrl, width, height);
-  };
-  x.onerror = function() {
-    errorCallback('Network error.');
-  };
-  x.send();
-}
 
-function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
-}
+    var ctx, flag = false,
+        prevX = 0,
+        currX = 0,
+        prevY = 0,
+        currY = 0,
+        prevtouchX = 0,
+        prevtouchY = 0,
+        touchX = 0,
+        touchY = 0;
 
-document.addEventListener('DOMContentLoaded', function() {
-  getCurrentTabUrl(function(url) {
-    // Put the image URL in Google search.
-    renderStatus('Performing Google Image search for ' + url);
+    canvas.width = document.body.clientWidth;
+    canvas.height = $(document).height();
+    ctx = canvas.getContext("2d");
+    var fromTop = document.body.scrollTop || document.documentElement.scrollTop;
 
-    getImageUrl(url, function(imageUrl, width, height) {
+    w = canvas.width;
+    h = canvas.height;
 
-      renderStatus('Search term: ' + url + '\n' +
-          'Google image search result: ' + imageUrl);
-      var imageResult = document.getElementById('image-result');
-      // Explicitly set the width/height to minimize the number of reflows. For
-      // a single image, this does not matter, but if you're going to embed
-      // multiple external images in your page, then the absence of width/height
-      // attributes causes the popup to resize multiple times.
-      imageResult.width = width;
-      imageResult.height = height;
-      imageResult.src = imageUrl;
-      imageResult.hidden = false;
+    window.onscroll = function() {
+      if (!stop) {
+        if($(document).scrollTop() > 32767) {
+          alert("Unfortunately due to chrome browser limits, Page Marker does not support pages with a height greater than 32,767 pixels.");
+          exit();
+        }
+        if($(document).height() != document.getElementById('can').height) {
+          var save = ctx.getImageData(0, 0, document.body.clientWidth, $(document).height());
+          document.getElementById('can').width = document.body.clientWidth;
+          document.getElementById('can').height = $(document).height();
+          w = document.getElementById('can').width;
+          h = document.getElementById('can').height;
+          ctx.putImageData(save, 0, 0);
+        }
+        var fromTop = document.body.scrollTop || document.documentElement.scrollTop;
+      }
+    };
 
-    }, function(errorMessage) {
-      renderStatus('Cannot display image. ' + errorMessage);
+    canvas.addEventListener("mousemove", function(e) {
+        findxy('move', e)
+    }, false);
+    canvas.addEventListener("mousedown", function(e) {
+        findxy('down', e)
+    }, false);
+    canvas.addEventListener("mouseup", function(e) {
+        findxy('up', e)
+    }, false);
+    canvas.addEventListener("mouseout", function(e) {
+        findxy('out', e)
+    }, false);
+    canvas.addEventListener('touchstart', sketchpad_touchStart, false);
+    canvas.addEventListener('touchmove', sketchpad_touchMove, false);
+    
+    $(document).keydown(function(e) {
+        if (!stop) {
+            switch (e.which) {
+                case 88:
+                    erase();
+                    break;
+                case 27:
+                    exit();
+                    break;
+            }
+        }
     });
-  });
-});
+    function draw() {
+        ctx.beginPath();
+        if(erase_drawing) {
+          ctx.globalCompositeOperation="destination-out";
+          ctx.lineWidth = thickness * 3;
+          ctx.strokeStyle = color;
+          ctx.lineJoin = "round";
+          ctx.moveTo(prevX, prevY);
+          ctx.lineTo(currX, currY);
+          ctx.closePath();
+          ctx.stroke();
+        } else {
+          ctx.globalCompositeOperation="source-over";
+          ctx.lineWidth = thickness;
+          ctx.strokeStyle = color;
+          ctx.lineJoin = "round";
+          ctx.moveTo(prevX, prevY);
+          ctx.lineTo(currX, currY);
+          ctx.closePath();
+          ctx.stroke();
+        }
+    }
+    function drawTouch(x, y) {
+        var thickness = document.getElementById("thickness").value;
+        var color = document.getElementById("color").value;
+        ctx.beginPath();
+        if(erase_drawing) {
+          ctx.globalCompositeOperation="destination-out";
+          ctx.lineWidth = thickness * 3;
+          ctx.strokeStyle = color;
+          ctx.lineJoin = "round";
+          ctx.moveTo(prevtouchX, prevtouchY);
+          ctx.lineTo(x, y);
+          ctx.closePath();
+          ctx.stroke();
+        } else {
+          ctx.globalCompositeOperation="source-over";
+          ctx.lineWidth = thickness;
+          ctx.strokeStyle = color;
+          ctx.lineJoin = "round";
+          ctx.moveTo(prevtouchX, prevtouchY);
+          ctx.lineTo(x, y);
+          ctx.closePath();
+          ctx.stroke();
+        }
+    }
+    function erase() {
+        ctx.clearRect(0, 0, w, h);
+    }
+    function eraser() {
+      erase_drawing = true;
+      document.getElementById("draw_button").style.background =  "rgba(0,0,0,0)";
+      document.getElementById("erase_button").style.background =  "rgba(0,0,0,0.2)";
+    }
+    function pen() {
+      erase_drawing = false;
+      document.getElementById("erase_button").style.background =  "rgba(0,0,0,0)";
+      document.getElementById("draw_button").style.background =  "rgba(0,0,0,0.2)";
+    }
+    function exit() {
+        stop = true;
+        canvas.removeEventListener("mousemove", false);
+        canvas.removeEventListener("mousedown", false);
+        canvas.removeEventListener("mouseup", false);
+        canvas.removeEventListener("mouseout", false);
+        canvas.removeEventListener("touchstart", false);
+        canvas.removeEventListener("touchmove", false);
+        document.getElementById('can').remove();
+        document.getElementById('draggable').remove();
+    }
+    function findxy(res, e) {
+        if (res == 'down') {
+            prevX = currX;
+            prevY = currY;
+            currX = e.pageX - canvas.offsetLeft;
+            currY = e.pageY - canvas.offsetTop;
+            var thickness = document.getElementById("thickness").value;
+            var color = document.getElementById("color").value;
+            ctx.beginPath();
+            if(!erase_drawing) {
+              ctx.globalCompositeOperation="source-over";
+              ctx.strokeStyle = color;
+              ctx.lineWidth = thickness;
+              ctx.lineJoin = "round";
+              ctx.moveTo(currX, currY-0.001);
+              ctx.lineTo(currX, currY);
+            } else {
+              ctx.globalCompositeOperation="destination-out";
+              ctx.strokeStyle = color;
+              ctx.lineWidth = thickness * 3;
+              ctx.lineJoin = "round";
+              ctx.moveTo(currX, currY-0.001);
+              ctx.lineTo(currX, currY);
+            }
+            ctx.closePath();
+            ctx.stroke();
+            flag = true;
+        }
+        if (res == 'up' || res == "out") {
+            flag = false;
+        }
+        if (res == 'move') {
+            if (flag) {
+                prevX = currX;
+                prevY = currY;
+                currX = e.pageX - canvas.offsetLeft;
+                currY = e.pageY - canvas.offsetTop;
+                draw();
+            }
+        }
+    }
+    function getTouchPos(e) {
+        if (!e)
+            var e = event;
+        if (e.touches) {
+            if (e.touches.length == 1) {
+                prevtouchX = touchX;
+                prevtouchY = touchY;
+                var touch = e.touches[0];
+                touchX = touch.pageX - touch.target.offsetLeft;
+                touchY = touch.pageY - touch.target.offsetTop;
+            }
+        }
+    }
+    function sketchpad_touchStart() {
+        getTouchPos();
+        prevtouchX = touchX;
+        prevtouchY = touchY;
+        drawTouch(touchX, touchY);
+        event.preventDefault();
+    }
+    function sketchpad_touchMove(e) {
+        getTouchPos();
+        drawTouch(touchX, touchY);
+        event.preventDefault();
+    }
+}
