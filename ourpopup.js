c45f6d9bdd60a7d0ae273b393662a1137dbcf039
Melvin Woo <MelvinWoo@Melvins-MacBook-Air.local>
2016-12-08 04:48:34 -0500

    hi

--- ourpopup.js
+++ ourpopup.js
@@ -16,10 +16,11 @@ if (document.getElementById('can') && document.getElementById('draggable')) {
     options.id = "draggable";
     document.body.appendChild(options);
     // layout options in options object
-    $("#draggable").append('<div>Web Draw</div><input type="hidden" name="color" id="color"><input type="button" value="Draw" img src="draw_icon.png" id=draw_button class="functionalities"><input type="button" value="Erase" id=erase_button class="functionalities"><input type="button" value="Highlight" id=highlight_button class="functionalities"><input type="button" value="Save Image" id=save_button class="functionalities"><input type="button" value="Clear" id=clear_button class="functionalities"><input type="button" value="Exit" id=exit_button class="functionalities"><input type="hidden" name = "thickness" id="thickness">');
-    
-    // # specifies that the elements are selected by their ID's
     $("#drawingCanvas").append('</canvas>');
+    $("#draggable").append('<div>Web Draw</div><input type="hidden" name="color" id="color"><a id="draw_icon"><img id="draw_iconImg" style="padding:0px" width="20px" height="20px"></img></a><input type="button" value="Erase" id=erase_button class="functionalities"><input type="button" value="Highlight" id=highlight_button class="functionalities"><input type="button" value="Save Image" id=save_button class="functionalities"><input type="button" value="Clear" id=clear_button class="functionalities"><input type="button" value="Exit" id=exit_button class="functionalities"><input type="hidden" name = "thickness" id="thickness">');
+    document.getElementById("draw_iconImg").src=chrome.extension.getURL("draw_icon.jpg");
+    document.getElementById("draw_icon").style.background = "rgba(0,0,0,0.2)";
+    // # specifies that the elements are selected by their ID's
 
     // when clicked, go to these functions
     $("#highlight_button").click(highlight);
@@ -39,9 +40,20 @@ if (document.getElementById('can') && document.getElementById('draggable')) {
       document.getElementById("thickness").value = items.pthickness;
       $("#color").val(items.pcolor);
     });
-    var draw_button = document.getElementById("draw_button");
+    var draw_icon = document.getElementById("draw_icon");
     var erase_button = document.getElementById("erase_button");
     var buttons = document.getElementsByClassName("functionalities");
+    
+    with(draw_icon.style) {
+      display = 'inline-block';
+      boxSizing = 'content-box';
+      margin = '5px';
+      padding = '4px';
+      width = '121px';
+      height = '120px';
+      borderRadius = '3px';
+      cursor = 'pointer';
+    }
     for (var i = 0; i < buttons.length; i++) {
         with(buttons[i].style) {
             padding = '5px';
