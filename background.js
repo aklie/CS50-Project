562b8d4e3cdc5cff8376650be8a538171b477cf6
Charles Pei <charlespei@Charless-MacBook-Pro-2.local>
2016-12-08 09:02:24 -0500

    final

--- background.js
+++ background.js
@@ -2,6 +2,7 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript(tab.id, {
         file: "jquery.min.js"
     }, function() {
+        // chrome extensions don't work on chrome extension pages, execute error alert
         if (chrome.runtime.lastError) {
             alert("Sorry! Web Doodle doesn't work on this page!");
         } else {
@@ -10,33 +11,27 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     });
 });
 
+// for html address
 var id =100;
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         switch (request.directive) {
         case "popup-click":
-            // execute the content script
-            chrome.tabs.captureVisibleTab(function(screenshotUrl) {
-    var viewTabUrl = chrome.extension.getURL('screenshot.html?id=' + id++)
-    var targetId = null;
+    // take screenshot when message is received by listener
+    chrome.tabs.captureVisibleTab(function(screenshotUrl) {
+      var viewTabUrl = chrome.extension.getURL('screenshot.html?id=' + id++)
+      var targetId = null;
 
     chrome.tabs.onUpdated.addListener(function listener(tabId, changedProps) {
-      // We are waiting for the tab we opened to finish loading.
-      // Check that the tab's id matches the tab we opened,
-      // and that the tab is done loading.
+      // Wait for the tab to open, check that the tab's id matches the tab and that the tab finished loading.
       if (tabId != targetId || changedProps.status != "complete")
         return;
 
-      // Passing the above test means this is the event we were waiting for.
-      // There is nothing we need to do for future onUpdated events, so we
-      // use removeListner to stop getting called when onUpdated events fire.
+      // Do not need listener anymore so remove to avoid issues
       chrome.tabs.onUpdated.removeListener(listener);
 
-      // Look through all views to find the window which will display
-      // the screenshot.  The url of the tab which will display the
-      // screenshot includes a query parameter with a unique id, which
-      // ensures that exactly one view will have the matching URL.
+      // Look through views to find screenshot window
       var views = chrome.extension.getViews();
       for (var i = 0; i < views.length; i++) {
         var view = views[i];
@@ -49,18 +44,14 @@ chrome.runtime.onMessage.addListener(
 
     chrome.tabs.create({url: viewTabUrl}, function(tab) {
       targetId = tab.id;
-    });
   });
+});
 
-            // chrome.tabs.executeScript(null, { // defaults to the current tab
-            //     file: "contentscript.js", // script to inject into page and run in sandbox
-            //     allFrames: true // This injects script into iframes in the page and doesn't work before 4.0.266.0.
-            // });
-            sendResponse({}); // sending back empty response to sender
-            break;
-        default:
-            // helps debug when request directive doesn't match
-            alert("Unmatched request of '" + request + "' from script to background.js from " + sender);
-        }
-    }
-);
+// send empty response to sender
+sendResponse({}); 
+  break;
+  default:
+  // debug when message request and sender do not match
+  alert("Unmatched request of '" + request + "' from script to background.js from " + sender);
+  }
+});
